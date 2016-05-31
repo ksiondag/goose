487dbd0dafe3541615a49ca28422e1054f34a6e0
Silent K <silentksionda@gmail.com>
2016-05-30 22:48:21 -0700

    Version 0.1.0 commit.
    Basic model setup.
    
    Will be usuable for prototypes, maybe, but work can not be saved, so any
    dynamic creation of model instances and such will be lost, only skeleton
    connection.
    
    See test.js for various functional capabilities.

--- index.js
+++ index.js
@@ -1,1 +1,415 @@
-// TODO
+'use strict';
+
+// TODO: 2016/05/29
+// Might be better to be capable of creating multiple seperate instances of
+// goose, as multiple different setups could be of use in a single app
+
+var models = {};
+
+exports.model = function (name, props) {
+    var Model = models[name];
+
+    if (Model) {
+        if (!Model.sameDefinition(props)) {
+            throw 'ModelError: Multiple different definitions of' + name;
+        }
+        return Model;
+    }
+
+    var propSet = {};
+    Model = function (obj) {
+        // TODO: 2016/05/29
+        // Imperfect object checking
+        // Doesn't verify that relationship members are of correct model
+        // type
+        this._id = null;
+        Object.keys(propSet).forEach((key) => {
+            if (!propSet.hasOwnProperty(key)) { return; }
+            Object.defineProperty(this, key, propSet[key]());
+        });
+
+        Object.keys(obj).forEach((key) => {
+            if (!obj.hasOwnProperty(key)) { return; }
+            if (!propSet.hasOwnProperty(key)) {
+                throw 'Model has no property "' + key + '"';
+            }
+            this[key] = obj[key];
+        });
+    };
+
+    var instances = [];
+    Model.instances = {
+        all: function () {
+            return instances;
+        },
+        filter: function (params) {
+            var results;
+
+            if (!params) {
+                params = {};
+            }
+            results = this.all().filter((instance) => {
+                return Object.keys(params).every((key) => {
+                    return params[key] === instance[key];
+                });
+            });
+
+            return results;
+        },
+        get: function (params) {
+            var results = this.filter(params);
+            if (results.length > 1) {
+                throw 'More than one value meets criteria';
+            } else if (results.length < 1) {
+                return null;
+            }
+            return results[0];
+        }
+    };
+
+    Model.addProp = function (key, type) {
+        if (type.relationship) {
+            propSet[key] = type(name, key);
+            return;
+        }
+
+        propSet[key] = function () {
+            var val = null;
+            return {
+                get: function () {
+                    return val;
+                },
+                set: function (newVal) {
+                    if (newVal === null) {
+                        val = newVal;
+                        return;
+                    }
+                    val = type(newVal);
+                },
+                enumerable: true
+            };
+        };
+
+        Model.instances.all().forEach((instance) => {
+            Object.defineProperty(this, key, propSet[key]());
+        });
+    };
+
+    models[name] = Model;
+    Object.keys(props).forEach((key) => {
+        if (!props.hasOwnProperty(key)) { return; }
+        Model.addProp(key, props[key]);
+    });
+
+    Model.sameDefinition = function (definition) {
+        // TODO: 2016/05/29
+        // Imperfect comparison of definitions
+        // Doesn't check that relationships are consistent
+
+        if (!definition) {
+            return true;
+        }
+
+        Object.keys(definition).forEach((key) => {
+            if (!definition.hasOwnProperty(key)) { return; }
+            if (!props.hasOwnProperty(key)) { 
+                return false;
+            }
+        });
+
+        Object.keys(props).forEach((key) => {
+            if (!props.hasOwnProperty(key)) { return; }
+            if (!definition.hasOwnProperty(key)) {
+                return false;
+            }
+        });
+    };
+
+    var id = 0;
+    Model.prototype.save = function () {
+        if (this._id !== null) {
+            return this;
+        }
+        this._id = id;
+        id += 1;
+        instances.push(this);
+        return this;
+    };
+
+    return Model;
+};
+
+var relationship = function (func) {
+    return function () {
+        var ret;
+        ret = func.apply(this, arguments);
+        ret.relationship = true;
+        return ret;
+    };
+};
+
+exports.oneToOne = relationship(function (otherName) {
+    var oneToOneAPI;
+    var OtherModel = models[otherName];
+
+    if (!OtherModel) {
+        throw 'No model with name ' + otherName;
+    }
+
+    oneToOneAPI = function (name, key) {
+        var Model = models[name];
+        var otherType = function () {
+            return function () {
+                return {
+                    get: function () {
+                        var search = {};
+                        search[key] = this;
+                        return Model.instances.get(search);
+                    },
+                    set: function (obj) {
+                        var search;
+                        var originalObj;
+
+                        search = {
+                            [key]: this._id
+                        };
+
+                        originalObj = Model.instances.get(search);
+
+                        if (originalObj) {
+                            originalObj[key] = null;
+                        }
+
+                        if (obj === null) {
+                            return;
+                        }
+
+                        if (!(obj instanceof Model)) {
+                            throw `Assigning non-${name} instance to ${otherName}.${key}`;
+                        }
+                        obj[key] = this;
+                    },
+                    enumerable: true
+                };
+            };
+        };
+        otherType.relationship = true;
+        OtherModel.addProp(name.toLowerCase(), otherType);
+
+        return function () {
+            var otherId = null;
+            return {
+                get: function () {
+                    if (otherId === null) {
+                        return null;
+                    }
+                    return OtherModel.instances.get({_id: otherId});
+                },
+                set: function (otherObj) {
+                    if (!otherObj) {
+                        otherId = null;
+                    }
+                    if (!(otherObj instanceof OtherModel)) {
+                        throw `Assigning non-${otherName} instance to ${name}.${key}`;
+                    }
+                    otherId = otherObj._id;
+                },
+                enumerable: true
+            };
+        };
+    };
+
+    return oneToOneAPI;
+});
+
+exports.manyToOne = relationship(function (otherName) {
+    var manyToOneAPI;
+    var OtherModel = models[otherName];
+
+    if (!OtherModel) {
+        throw 'No model with name ' + otherName;
+    }
+
+    manyToOneAPI = function (name, key) {
+        var Model = models[name];
+        var otherType = function () {
+            return function () {
+                return {
+                    get: function () {
+                        var search = {};
+                        search[key] = this;
+                        return Model.instances.filter(search);
+                    },
+                    enumerable: true
+                };
+            };
+        };
+        otherType.relationship = true;
+        OtherModel.addProp(name.toLowerCase() + 's', otherType);
+
+        OtherModel.prototype['add' + name] = function (obj) {
+            obj[key] = this;
+        };
+
+        OtherModel.prototype['remove' + name] = function (obj) {
+            if (obj && obj[key] === this) {
+                obj[key] = null;
+            }
+        };
+
+        return function () {
+            var otherId = null;
+            return {
+                get: function () {
+                    if (otherId === null) {
+                        return null;
+                    }
+                    return OtherModel.instances.get({_id: otherId});
+                },
+                set: function (otherObj) {
+                    if (!otherObj) {
+                        otherId = null;
+                    }
+                    if (!(otherObj instanceof OtherModel)) {
+                        throw `Assigning non-${otherName} instance to ${name}.${key}`;
+                    }
+                    otherId = otherObj._id;
+                },
+                enumerable: true
+            };
+        };
+    };
+
+    return manyToOneAPI;
+});
+
+var manyMaps = {};
+exports.manyToMany = relationship(function (otherName) {
+    // TODO 2016/05/30
+    var manyToManyAPI;
+    var OtherModel = models[otherName];
+
+    if (!OtherModel) {
+        throw 'No model with name ' + otherName;
+    }
+
+    manyToManyAPI = function (name, key) {
+        var Model = models[name];
+        var otherType = function () {
+            return function () {
+                return {
+                    get: function () {
+                        return Model.instances.all().filter((instance) => {
+                            return instance[key].some((otherInstance) => {
+                                return otherInstance === this;
+                            });
+                        });
+                    },
+                    set: function (objs) {
+                        var oldObjs = this[name.toLowerCase() + 's'];
+                        oldObjs.forEach((obj) => {
+                            obj['remove' + otherName](this);
+                        });
+                        objs.forEach((obj) => {
+                            obj['add' + otherName](this);
+                        });
+                    },
+                    enumerable: true
+                };
+            };
+        };
+        otherType.relationship = true;
+        OtherModel.addProp(name.toLowerCase() + 's', otherType);
+
+        OtherModel.prototype['add' + name] = function (obj) {
+            if(!(obj instanceof Model)) {
+                throw `Assigning non-${name} instance to ${otherName}.${key}`;
+            }
+            obj['add' + otherName](this);
+        };
+
+        OtherModel.prototype['remove' + name] = function (obj) {
+            obj['remove' + otherName](this);
+        };
+
+        Model.prototype['add' + otherName] = function (otherObj) {
+            var otherObjs;
+            if(!(otherObj instanceof OtherModel)) {
+                throw `Assigning non-${otherName} instance to ${name}.${key}`;
+            }
+
+            otherObjs = this[key];
+            // Other object already present in list, do nothing
+            if(otherObjs.some((check) => check === otherObj)) {
+                return;
+            }
+            otherObjs.push(otherObj);
+            this[key] = otherObjs;
+        };
+
+        Model.prototype['remove' + otherName] = function (otherObj) {
+            var index, otherObjs;
+            otherObjs = this[key];
+            index = otherObjs.indexOf(otherObj);
+            if (index === -1) {
+                return;
+            }
+            otherObjs.splice(index, 1);
+            this[key] = otherObjs;
+        };
+
+        return function () {
+            var otherIds = [];
+            return {
+                get: function () {
+                    return otherIds.map((id) => {
+                        return OtherModel.instances.get({_id: id});
+                    });
+                },
+                set: function (otherObjs) {
+                    otherIds = otherObjs.map((otherObj) => otherObj._id);
+                },
+                enumerable: true
+            };
+        };
+    };
+
+    return manyToManyAPI;
+});
+
+exports.purge = function () {
+    models = {};
+};
+
+exports.dump = function (filename) {
+    var name, Model, instances, dumpJSON = [];
+    for (name in models) {
+        if (!models.hasOwnProperty(name)) { continue; }
+        Model = models[name];
+        instances = Model.instances.all();
+        instances.forEach((instance) => {
+            var key, val, dumpRow;
+            dumpRow = {
+                name: name,
+                id: instance._id,
+                properties: {}
+            };
+            for (key in instance) {
+                if (key === '_id' || !instance.hasOwnProperty(key)) {
+                    continue;
+                }
+                val = instance[key];
+                if (val._id) {
+                    dumpRow.properties[key] = val._id;
+                    continue;
+                }
+                dumpRow.properties[key] = val;
+            }
+            dumpJSON.push(dumpJSON);
+        });
+    }
+};
+
+exports.load = function (filename) {
+};
+
--- readme.md
+++ readme.md
@@ -1,11 +1,49 @@
 # Goose
 
-API that plugs into databases.
+Model creation API inspired from [Django](https://www.djangoproject.com/) and
+[Mongoose](http://mongoosejs.com/).
 
-Well, that'll be what it is eventually. For right now, it'll be an API that
-maps models to sqlite3 databases.
+## Why Goose?
 
-Basically, I like [mongoose](http://mongoosejs.com/), but want to be able to
-use the setup with any database I so choose, like with
-[Django's](https://www.djangoproject.com/) model setup.
+Original idea was to separate [Mongoose](http://mongoosejs.com/)'s model
+abstraction from [mongoDB](https://www.mongodb.com/). Hence the name "Goose". I
+might want to have models that don't live in a mongo database. I might not want
+to use mongoDB at all, or maybe even not use a database at all but still use
+the model abstraction.
+
+If I'm just prototyping an idea really fast, I don't need the baggage of
+thinking about scaling and databases. I just want to connect things together.
+Even needing to setup and interact with a [SQLite](https://www.sqlite.org/)
+database can be distracting from the idea.
+
+Then, when scaling comes into play, I'd like to measure the different database
+options via proper profiling (not that I know how to do that). Meaning I'd like
+to be able to plug my implementation into a database via wrappers and such.
+Hopefully, the wrapping doesn't incur significant resourse-cost overall.
+
+## Roadmap
+
+### Model implementation, no database (Version 0.x.0)
+
+1. Model-creation and interactivity with basic tests using core node library
+1. Model dumping to and loading from file
+1. Natural keys and model property requirements
+   * no null
+   * type-enforcement
+   * etc.
+
+### Modularity and Database Plugin Socket (Version 1.x.0)
+
+1. Model logic removed from main goose area and modularized elsewhere
+1. Plugin capability to replace in-memory model storage to any given database
+   wrapper
+   * goose-sqlite (separate library) proof-of-concept
+   * goose-mongodb (separate library)
+     * come full circle with mongoose
+     * compare/contrast with mongoose
+
+### Profiling Tool (Version 2.x.0)
+
+??? (I have no idea how to do proper profiling stuff, let alone build it into
+a library.)
 
